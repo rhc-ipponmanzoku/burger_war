e8fd4338dca2a92a4aefecb031d3010362d6c622
miitac <69803003+miitac@users.noreply.github.com>
2020-08-31 04:46:01 +0900

    Update testRun.py

--- burger_war/scripts/testRun.py
+++ burger_war/scripts/testRun.py
@@ -18,6 +18,7 @@ from std_msgs.msg import String
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
+import json
 
 class QrReader(object):
 
@@ -33,6 +34,8 @@ class QrReader(object):
 
         # publish marked qr area image
         self.qr_img_pub = rospy.Publisher('qr_image', Image, queue_size=1)
+
+
 
     # comvert image topic to opencv object and show
     def imageCallback(self, data):
@@ -51,15 +54,63 @@ class QrReader(object):
                 self.qr_val_pub.publish(str(i[0]))
                 print str(i[0])
 
+
+
         im_msg = self.bridge.cv2_to_imgmsg(im, "bgr8")
         self.qr_img_pub.publish(im_msg)
 
+
 class testBot():
+
     def __init__(self):
         # velocity publisher
         self.vel_pub = rospy.Publisher('cmd_vel', Twist,queue_size=1)
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
 
+        # score subscriver
+        self.myColor     = None
+        self.myScore     = 0
+        self.enemyScore  = None
+        self.warState    = None
+        self.score_sub   = rospy.Subscriber('/war_state', String, self.warStateCallback, queue_size=1)
+
+        ############
+        ##   QR   ##
+        ############
+        # for convert image topic to opencv obj
+        self.bridge = CvBridge()
+
+        # camera subscriver
+        self.image_sub = rospy.Subscriber('/image_raw', Image, self.imageCallback, queue_size=1)
+
+        # publish qr_val
+        self.qr_val_pub = rospy.Publisher('qr_val', String, queue_size=1)
+
+        # publish marked qr area image
+        self.qr_img_pub = rospy.Publisher('qr_image', Image, queue_size=1)
+
+    #warState callback
+    def warStateCallback(self,data):
+        warState = data
+        jsonWarState = json.loads(warState.data)
+        self.warState = jsonWarState["scores"]
+        print self.warState
+
+        # which team?
+        if jsonWarState["players"]["r"] == "you":
+            #print " you are red"
+            self.myColor = "r"
+        else:
+            #print " you are blue"
+            self.myColor = "b"
+
+        #update myScore
+        self.myScore = jsonWarState["scores"][self.myColor]
+        print self.myScore
+
+        #print jsonWarState["players"]["r"]
+        #print self.warState["b"]
+        #self.warState = jsonWarState.keys()
 
     def setGoal(self,x,y,yaw):
         self.client.wait_for_server()
@@ -78,12 +129,38 @@ class testBot():
         goal.target_pose.pose.orientation.w = q[3]
 
         self.client.send_goal(goal)
-        wait = self.client.wait_for_result()
-        if not wait:
-            rospy.logerr("Action server not available!")
-            rospy.signal_shutdown("Action server not available!")
-        else:
-            return self.client.get_result()        
+#        wait = self.client.wait_for_result()
+#        if not wait:
+#            rospy.logerr("Action server not available!")
+#            rospy.signal_shutdown("Action server not available!")
+#        else:
+#            return self.client.get_result()        
+
+    def imageCallback(self, data):
+        try:
+            im = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        # read AR code
+        aruco = cv2.aruco
+        dictionary = aruco.getPredefinedDictionary(aruco.DICT_7X7_50)
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(im, dictionary)
+        aruco.drawDetectedMarkers(im, corners, ids)
+        if ids is not None:
+            print "find ID!!!"
+            for i in ids:
+                self.qr_val_pub.publish(str(i[0]))
+                print str(i[0])
+
+                print next_point
+                if next_point < [len(v) for v in goal_point][0] :
+                    state = "move_goal"
+                    next_point = next_point + 1
+
+
+        im_msg = self.bridge.cv2_to_imgmsg(im, "bgr8")
+        self.qr_img_pub.publish(im_msg)
 
     def calcTwist(self,x,th,run_time):
 
@@ -94,39 +171,45 @@ class testBot():
         return twist
 
     def strategy(self):
-        r = rospy.Rate(50) # change speed 50fps
+        r = rospy.Rate(10) # change speed 10fps
 
-        #move left side
-        self.setGoal(-0.84,-0.51,0)
-        
-        self.setGoal(-0.34,0.02,0)
-        
-        self.setGoal(-0.9,0.52,0)
-        
-        self.setGoal(-0.37,0.51,0)
-        self.setGoal(-0.37,0.51,3.1415/4)
-        
-        self.setGoal(-0.84,0.31,3.1415/2)
-        
-        #scan QR
-        print "start QR read"
-        rospy.spin()
-        print "end QR read"
+        goal_point = [ [-0.84,-0.51],#first(x,y)
+                       [-0.47,0.02],#second(x,y)
+                       [ -0.9,0.52], #third(x,y)
+                       [-0.37,0.51] 
+                      ]
+        next_spot = 0
+        now_score = 0
+        state = "move_goal"
 
-        #move right side
-        self.setGoal(-0.34,0.02,0)
+        while not rospy.is_shutdown(): 
+            if state == "move_goal":
+                self.setGoal(goal_point[next_spot][0],
+                             goal_point[next_spot][1],
+                             0)
+                print "next_spot : " + str(next_spot)
+                state = "wait_get_point"
+                print "waiting"
+            elif state == "wait_get_point":
+                state = "wait_get_point"
 
-        self.setGoal(0.84,-0.51,0)
-        
-        self.setGoal(0.34,0.02,0)
-        
-        self.setGoal(0.9,0.52,0)
-        
-        self.setGoal(0.37,0.51,0)
-        self.setGoal(0.37,0.51,-3.1415/4)
-        
-        self.setGoal(0.84,0.31,-3.1415/2)
+                #reach target goal  or
+                if (now_score+1) == int(self.myScore):
+                    now_score = int(self.myScore)
+                    print "Next Spot !!!!!!"
+                    state = "move_goal"
+                    if next_spot < len(goal_point):# [len(v) for v in goal_point][0]:
+                        next_spot = next_spot + 1
+                        self.client.cancel_goal()
 
+        
+#        self.setGoal(-0.34,0.02,0)
+        
+#        self.setGoal(-0.9,0.52,0)
+        
+#        self.setGoal(-0.37,0.51,0)
+#        self.setGoal(-0.37,0.51,3.1415/4)
+  
         #twist = self.calcTwist(0.5,0,3)
         #self.vel_pub.publish(twist)
         #rospy.sleep(0.5)
@@ -159,6 +242,6 @@ class testBot():
 if __name__ == '__main__':
     rospy.init_node('test_run')
     bot = testBot()
-    qr = QrReader()
+#    qr = QrReader()
     bot.strategy()
 
